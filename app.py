cbe4ff6e1d6a56b6dc7f197e40d93e96fb3a4a56
Filipe Morais <afilipem@gmail.com>
2021-08-31 22:50:11 -0500

    redefines functions to get the code more efficient

--- app.py
+++ app.py
@@ -9,12 +9,22 @@ Your selection: """
 
 Welcome = "Welcome to the programming diary!"
 
+def promt_new_entry():
+    entry_content = input("What have you learned today?  ")
+    entry_date = input ("Enter the date: ")
+
+    add_entry(entry_content, entry_date)
+
+def view_entries (entries):
+    for entry in entries:
+        print(f"{entry['date']}\n{entry['content']}\n\n")
+
 print (Welcome)
 
 while (user_input := input(menu)) != "3":
     if user_input == "1":
-        print("Adding...")
+        prompt_new_entry()
     elif user_input == "2":
-        print("Viewing...")
+        view_entries(get_entries())
     else:
         print("Invalid option, please try again!")  
