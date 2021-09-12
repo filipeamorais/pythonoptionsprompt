74b850189d50cc03fbea869ddaa39b762132576b
Filipe Morais <afilipem@gmail.com>
2021-09-11 21:54:46 -0500

    adds sqlite library and functions to create a table

--- database.py
+++ database.py
@@ -1,4 +1,11 @@
-entries = []
+import sqlite3
+from sqlite3.dbapi2 import connect
+
+connection = sqlite3.connect("data.db")
+
+def create_table():
+    with connection:
+        connection.execute("CREATE TABLE entries (content TEXT,date TEXT )")
 
 def add_entry (entry_content, entry_date):
     entries.append({"content": entry_content, "date":entry_date})
